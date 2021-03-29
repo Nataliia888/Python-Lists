4df02a0b31cc9a9cffe6d3eb97fad064e33857a0
Nataliia888 <80485127+Nataliia888@users.noreply.github.com>
2021-03-28 22:59:21 -0400

    Add files via upload

---
+++ working_with_list.py
@@ -0,0 +1,9 @@
+grocery_list = ['Juice', 'Tomato', 'bread', 'Milk']
+ 
+print('First item', grocery_list[0])
+grocery_list[0] = "Orange"
+print('First item', grocery_list[0])
+other_events = ['Wash car', 'Pick up kids']
+to_do_list = [other_events, grocery_list]
+print((to_do_list[1][1]))
+
